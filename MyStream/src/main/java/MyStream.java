9a2c4f5143444b717a808a2cbad9ef38ea421a00
Egor <shurov13@mail.ru>
2019-01-10 13:36:52 +0300

    faet: MyStream

---
+++ MyStream/src/main/java/MyStream.java
@@ -0,0 +1,31 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+@Data
+@AllArgsConstructor
+public class MyStream<T> {
+
+    private List<T> myStreamContent;
+
+    MyStream<T> filter(Predicate<T> predicate) {
+        List<T> newCollection = new ArrayList<>();
+        for (T value : myStreamContent) {
+            if (predicate.test(value)) {
+                newCollection.add(value);
+            }
+        }
+        return new MyStream<T>(newCollection);
+    }
+
+    <R> MyStream<R> map(Function<T, R> function) {
+        List<R> newCollection = new ArrayList<>();
+        for (T value : myStreamContent) {
+            newCollection.add(function.apply(value));
+        }
+        return new MyStream<R>(newCollection);
+    }
+}
